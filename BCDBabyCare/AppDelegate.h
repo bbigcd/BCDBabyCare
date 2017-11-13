d98d3704cd13c79b5aa304e0599772d7e4553e93
bbigcd <bbigcd@163.com>
2017-11-13 22:54:47 +0800

    Initial Commit

---
+++ BCDBabyCare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BCDBabyCare
+//
+//  Created by chendi on 2017/11/13.
+//  Copyright © 2017年 bbigcd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BCDBabyCare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BCDBabyCare
+//
+//  Created by chendi on 2017/11/13.
+//  Copyright © 2017年 bbigcd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
